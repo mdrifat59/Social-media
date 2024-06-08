b30f18c181b19e6fea2fb40dc8f61aad1c3cb77b
mdrifat59 <mdrifatulislam59@gmail.com>
2024-06-08 19:54:41 +0600

    Route setup in App.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'  
 import Registration from './pages/registration'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 
 function App() { 
 
+  const router = createBrowserRouter(
+      createRoutesFromElements(
+          <Route>
+            <Route path='/' element={<Registration/>}></Route>
+          </Route>
+      )
+  )
   return (
     <>
-      <Registration/>
+      <RouterProvider router={router}/>
     </>
   )
 }
